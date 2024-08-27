ec19a35db4373276f7ffce22a1cd1aedbe566900
suhanask003 <suhana2005.sk@gmail.com>
2024-08-27 20:16:43 +0530

    added images

--- src/components/News.js
+++ src/components/News.js
@@ -3,10 +3,6 @@ import NewsItem from './NewsItem';
 
 export class News extends Component {
   articles = [
-    {
-      "status": "ok",
-      "totalResults": 2,
-      "articles": [
         {
           "source": {
             "id": "espn-cric-info",
@@ -40,9 +36,9 @@ export class News extends Component {
           },
           "author": "NBC Sports",
           "title": "Jerod Mayo sounded mostly positive about Jacoby Brissett’s shoulder injury - NBC Sports",
-          "description": null,
+          "description": "Patriots quarterback, and presumptive Week 1 starter, Jacoby Brissett suffered a shoulder injury during his lone drive of Sunday night’s preseason finale, at Washington. Coach Jerod Mayo sounded mostly positive after the game, but there was a potential caveat in Mayo’s comments.",
           "url": "https://news.google.com/rss/articles/CBMi0AFBVV95cUxNU2tIYjlKMDZ1RDViTFFuTEdGMkpRZGN1U3V4RW5jd0trWHhwVmxEV2JhRl9uUGZRX2NVUXhQZWRfdVl1akdxOXJ3dnlZdzd5bTljRUJmOW15dzYyMDM2aGxaWW1FZUV3RzdJU3BERGtiMWJYQ3k1TUQwTDFHaHQzaU5NZWZRM2ZXemhSUmRlZDBDeDhpenpldUpEcG9KWkg2NFJNUURNYkF5X2trY3M3S1VEdkNEeGNkcnN4ZExKUjhialZIS2U0d01pd0ZKMzNS?oc=5",
-          "urlToImage": null,
+          "urlToImage":"https://www.washingtonpost.com/resizer/MeGFFLJZINBpnH8JEK_HWevLJ9o=/arc-anglerfish-washpost-prod-washpost/public/7GUDLGQP3UI6VESMWNGQTO6JJA.jpg" ,
           "publishedAt": "2024-08-26T09:25:56Z",
           "content": null
         },
@@ -53,21 +49,22 @@ export class News extends Component {
           },
           "author": "Sports Illustrated",
           "title": "Keegan Bradley Continues Surprise FedEx Cup Playoff Run With Win at BMW Championship - Sports Illustrated",
-          "description": null,
+          "description": "Playing this week—last week I was looking at the leaderboard the whole time, and this week that was never on my mind. I was just trying to win the tournament. Maybe I can. I'm playing great. I feel very lucky to be in Atlanta. To make the Tour Championship two years in a row is a big deal.",
           "url": "https://news.google.com/rss/articles/CBMihAFBVV95cUxQaG1DSC1ydXgxTk92NTdtODB3dDBZQ2MwdDRNaHRpdVN5RXpPcFNvRW1Vem9tVUoyY2ZsNEx4eVQ1RnJtMGlxVEJybXpWQnZEZ09QQ0RJUzN3QlN4VUpaMkNIbFQ4QjA0RkxMVXdrQXpzYi1uR0lMc1JWUTJHNkJvdjdFdi0?oc=5",
-          "urlToImage": null,
+          "urlToImage": "https://goqii.com/blog/wp-content/uploads/6-Health-Benefits-of-Playing-Sports-1024x683.jpg",
           "publishedAt": "2024-08-26T02:40:42Z",
           "content": null
         }
+      
       ]
-    }
-  ]
-  constructor() {
+    
+  
+  constructor(){
     super();
     console.log("Hello I am a constructor from News Componenent");
     this.state = {
       articles: this.articles,
-      loadng: false
+      loading: false
     }
   }
 
@@ -77,8 +74,8 @@ export class News extends Component {
         <h2>NewsMonkey - Top Headlines</h2>
         <div className="row">
           {this.state.articles.map((element) => {
-            return <div className="col-md-4" key = {element.url}>
-              <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+            return <div className="col-md-4" key = {element.url} >
+              <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url} />
             </div>
           })}
         </div>
@@ -86,5 +83,4 @@ export class News extends Component {
     )
   }
 }
-
 export default News
